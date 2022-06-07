4e741861dd9e99a7ee6adaffaf13eaecc8ac5406
Winter <rrcaseyr@gmail.com>
2022-06-06 23:18:05 -0500

    my custom date picker button works!

--- app/src/main/java/com/example/cosplayplanner/EditFragment.kt
+++ app/src/main/java/com/example/cosplayplanner/EditFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.cosplayplanner.databinding.FragmentEditBinding
 import com.example.cosplayplanner.models.Cosplay
 
